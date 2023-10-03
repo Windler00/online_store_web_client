b3f3f55b8e23243a7df7609f4121a6de673601d1
Windler00 <Windler00@gmail.com>
2023-10-03 13:37:22 +0600

    removed mui from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,32 @@
 import { useEffect } from "react"
 import ProductStore from "../store/ProductStore"
 import { observer } from "mobx-react-lite"
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import styles from './home.module.css'
 
 const Home = observer(() => {
     const HandleProducts = observer(() => {
+        const image = "https://img.ixbt.site/live/topics/preview/00/01/67/69/c989bed929.png"
         let result: any[] = [];
         if (ProductStore.products !== undefined) {
             ProductStore.products?.map((product =>
                 result.push(
-                    <div key={product.id} className={styles.ProductCard}>
-                        <Link to={"/product/" + product.id} style={{ textDecoration: "none" }} >
-                            <Card>
-                                <CardMedia
-                                    component="img"
-                                    height="200"
-                                    image="https://img.ixbt.site/live/topics/preview/00/01/67/69/c989bed929.png"
-                                    alt={product.name}
-                                />
-                                <CardContent>
-                                    <Typography variant="h5" component="div">
-                                        {product.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {product.description}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Link>
-                    </div>
+                    <>
+                        <Link key={product.id} to={"/product/" + product.id}>
+                            <div className={styles.ProductCard}>
+                                <img src={image} alt="Product image" />
+                                <h3>{product.name}</h3>
+                                <p>{product.description}</p>
+                            </div>
+                        </Link >
+                    </>
                 )
             ))
         }
         return (
-            <div>
+            <>
                 {result}
-            </div>
+            </>
         )
     })
 
