1748e12874680ab132f9290858744b916e091ac1
Windler00 <Windler00@gmail.com>
2024-01-24 23:56:26 +0600

    added bootstrap to basket page

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -5,7 +5,7 @@ import BasketStore from "../../store/BasketStore"
 import AuthStore from "../../store/AuthStore"
 import { useNavigate } from "react-router-dom"
 import Product from "../../components/Product/Product"
-
+import styles from './basket.module.css'
 
 const Basket = observer(() => {
     let navigate = useNavigate();
@@ -55,36 +55,61 @@ const Basket = observer(() => {
         }
     }
 
-    const handlerChangeCurrentPage = async (event: any) => {
-        if (event.target.value >= 1 && event.target.value <= BasketStore.pages) {
-            BasketApi.getProducts(event.target.value, BasketStore.pageSize)
+    const handlerChangeCurrentPage = async (value: any) => {
+        if (value >= 1 && value <= BasketStore.pages) {
+            BasketApi.getProducts(value, BasketStore.pageSize)
         }
     }
 
 
     return (
         <div>
-            <div>
-                {BasketStore.products.map((product) => (
-                    <div>
-                        <Product product={product.product} />
-                        <button onClick={() => decreaseQuantity(product.product.id, product.quantity - 1)}>-</button>
-                        <div>{product.quantity}</div>
-                        <button onClick={() => increaseQuantity(product.product.id, product.product.quantity, product.quantity + 1)}>+</button>
-                        <input value={product.product.id} onChange={handleSelectedProducts} type="checkbox"></input>
+            <div className={styles.BasketItems}>
+                {BasketStore.products.map((product, index) => (
+                    <div className={styles.BasketItem}>
+                        <Product key={index} product={product.product} />
+                        <div className={styles.BasketItemQuantity}>
+                            <div>
+                                <button className="btn btn-dark m-1" onClick={() => decreaseQuantity(product.product.id, product.quantity - 1)}>-</button>
+                            </div>
+                            <div className="m-1">{product.quantity}</div>
+                            <div>
+                                <button className="btn btn-dark m-1" onClick={() => increaseQuantity(product.product.id, product.product.quantity, product.quantity + 1)}>+</button>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
             <div>
-                <button>Buy</button>
+                <button className="btn btn-dark m-3">Buy</button>
             </div>
-            <div>
-                {BasketStore.currentPage === 1 ? (<button disabled>Previous</button>) : <button onClick={() => decreasePage()}>Previous</button>}
-                <input type="text" value={BasketStore.currentPage} onChange={handlerChangeCurrentPage}></input>
-                {BasketStore.currentPage === BasketStore.pages ? <button disabled>Next</button> : <button onClick={() => increasePage()}>Next</button>}
-                <p>Pages 1 - {BasketStore.pages}</p>
+            <div className={styles.PagesNav}>
+                {BasketStore.currentPage === 1 ? (<button className="btn btn-dark" disabled>Previous</button>) : <button className="btn btn-dark" onClick={() => decreasePage()}>Previous</button>}
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        {
+                            Array.from({ length: BasketStore.pages }, (_, index) => (
+                                BasketStore.currentPage === index + 1 ?
+                                    (<li className="page-item" key={index}><button disabled className="btn btn-dark" onClick={() => handlerChangeCurrentPage(index + 1)}>{index + 1}</button></li>)
+                                    : index + 1 === 1 ?
+                                        (<li className="page-item" key={index}><button className="btn btn-dark" onClick={() => handlerChangeCurrentPage(index + 1)}>{index + 1}</button></li>)
+                                        : index + 1 === BasketStore.pages ?
+                                            (<li className="page-item" key={index}><button className="btn btn-dark" onClick={() => handlerChangeCurrentPage(index + 1)}>{index + 1}</button></li>)
+                                            : BasketStore.currentPage === index ?
+                                                (<li className="page-item" key={index}><button className="btn btn-dark" onClick={() => handlerChangeCurrentPage(index + 1)}>{index + 1}</button></li>)
+                                                : BasketStore.currentPage === index + 1 ?
+                                                    (<li className="page-item" key={index}><button className="btn btn-dark" onClick={() => handlerChangeCurrentPage(index + 1)}>{index + 1}</button></li>)
+                                                    : BasketStore.currentPage === index + 2 ?
+                                                        (<li className="page-item" key={index}><button className="btn btn-dark" onClick={() => handlerChangeCurrentPage(index + 1)}>{index + 1}</button></li>)
+                                                        :
+                                                        (null)
+                            ))
+                        }
+                    </ul>
+                </nav>
+                {BasketStore.currentPage === BasketStore.pages ? <button className="btn btn-dark" disabled>Next</button> : <button className="btn btn-dark" onClick={() => increasePage()}>Next</button>}
             </div>
-        </div>
+        </div >
     )
 })
 
